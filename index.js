3b6445a1e7dc4be2fb97a52132d60964127e665b
Sarthak Parikh <sarthakparikh20010409@gmail.com>
2025-03-27 13:36:48 -0400

    created project

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+const Hello = (name) => {
+    console.log('Hello World!', name);
+};
+Hello('John Doe'); // Hello World! John Doe
---
+++ index.ts
@@ -0,0 +1,6 @@
+const Hello =(name:String) => {
+    console.log('Hello World!', name);
+    
+}
+
+Hello('John Doe'); // Hello World! John Doe
