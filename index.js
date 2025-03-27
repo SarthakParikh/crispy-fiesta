34463dfc39445d9143183778c38b27671a160b3f
Sarthak Parikh <sarthakparikh20010409@gmail.com>
2025-03-27 13:39:36 -0400

    Commit - 1 V1

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const Hello = (name) => {
-    console.log('Hello World!', name);
-};
-Hello('John Doe'); // Hello World! John Doe
--- index.ts
+++
@@ -1,6 +0,0 @@
-const Hello =(name:String) => {
-    console.log('Hello World!', name);
-    
-}
-
-Hello('John Doe'); // Hello World! John Doe
